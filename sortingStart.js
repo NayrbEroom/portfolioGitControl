922905831bc2ca61f53875c62c1b80b67dc37082
NayrbEroom <moorebryan21@gmail.com>
2020-05-16 22:10:09 -0500

    Create sortingStart.js
    bubblesort start

---
+++ sortingStart.js
@@ -0,0 +1,19 @@
+bubbleSort = (inputArr) => {
+
+  //set initial length of array passed into function
+  var len = inputArr.length;
+
+
+  //for loop that goes through the input array
+  //while i is less than length go through add one to i each time
+  for (let i = 0; i < len; i++) {
+    //then we set a second array to do the same thing as above loop through the entire inputArr
+    //but as we go through we will take the number that is at i and the one that is at j (j will be +1 to i) and compare them
+    for (let j = 0; j < len; j++)
+  }
+
+
+
+
+  return arr
+}

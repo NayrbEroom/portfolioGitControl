4f2b1a617b88a2ca0508b356e60b4bdaff0e6320
NayrbEroom <moorebryan21@gmail.com>
2020-05-17 18:21:33 -0500

    able to display multiple sketches TODO: make display prettier in css, connect sketches to each sorting algorithm

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 
 //assign it like a variable so you can do multiple sketches in one file
 //it will also allow me to control the canvas with html/css
-let canvasSketch = function(controller) {
+//instructions for assigning multiple sketches: http://www.joemckaystudio.com/multisketches/
+
+let canvasSketch1 = function(controller) {
 
   let canvasX = 100;
   let canvasY = 100;
@@ -16,4 +18,23 @@ let canvasSketch = function(controller) {
   };
 };
 
-let myp5 = new p5(canvasSketch, 'sketch-holder');
+var myp5 = new p5(canvasSketch1, 'sketch1');
+
+let canvasSketch2 = function(controller) {
+
+  let canvasX = 100;
+  let canvasY = 100;
+  controller.setup = function() {
+    controller.createCanvas(700,410);
+  };
+
+  controller.draw = function() {
+    controller.background(0);
+    controller.fill(255);
+    controller.rect(canvasX,canvasY, 50, 50);
+
+  };
+};
+
+
+var myp5 = new p5(canvasSketch2, 'sketch2');

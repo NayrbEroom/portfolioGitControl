06237c8cd20afe95c48f62e70b50e9b9f077e804
NayrbEroom <moorebryan21@gmail.com>
2020-05-16 22:57:24 -0500

    Update sortingStart.js
    finished bubblesort commented and tested

--- sortingStart.js
+++ sortingStart.js
@@ -1,7 +1,8 @@
 bubbleSort = (inputArr) => {
-
+  
   //set initial length of array passed into function
   var len = inputArr.length;
+
 
 
   //for loop that goes through the input array
@@ -9,11 +10,23 @@ bubbleSort = (inputArr) => {
   for (let i = 0; i < len; i++) {
     //then we set a second array to do the same thing as above loop through the entire inputArr
     //but as we go through we will take the number that is at i and the one that is at j (j will be +1 to i) and compare them
-    for (let j = 0; j < len; j++)
+    for (let j = i + 1; j < len; j++) {
+      //if first number is greater than the second number swap'em, else do nothing
+      if (inputArr[i] > inputArr[j]) {
+        //save the second number if above is true so when we swap we dont forget about it
+        let temp = inputArr[j];
+        //swap the two numbers
+        inputArr[j] = inputArr[i];
+        //put back in the saved unforgotten number
+        inputArr[i] = temp;
+      }
+    }
   }
 
 
 
 
-  return arr
+  return inputArr
 }
+
+//console.log(bubbleSort([124,82,101,-26,6,12342]));

f5069d14df9cda29e040d2abc020571faa608455
NayrbEroom <moorebryan21@gmail.com>
2020-06-11 23:30:15 -0500

    ahhhh we're gonna call this the beta run im gonna restart with more mobile first version in mind and use css grid

--- javascript/globalFunctions.js
+++ javascript/globalFunctions.js
@@ -6,3 +6,19 @@ function copyToClipboard(element) {
   $temp.remove();
   //copied https://codepen.io/shaikmaqsood/pen/XmydxJ
 }
+
+function parallax_height() {
+  var scroll_top = $(this).scrollTop();
+  var body_contetnt_top = $(".body-content").offset().top;
+  var navbar_height = $(".navbar").outerHeight();
+  $(".body-content").css({ "margin-top": navbar_height });
+  $(".navbar").css({ height: navbar_height - scroll_top });
+  //copied https://codepen.io/webmadewell/pen/zppMBX
+}
+parallax_height();
+$(window).scroll(function() {
+  parallax_height();
+});
+$(window).resize(function() {
+  parallax_height();
+});
